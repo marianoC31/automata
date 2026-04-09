1fb36f700f4f9767e285609030c601b2a1358e38
marianoC31 <mariano05055@gmail.com>
2026-04-09 00:07:16 -0600

    Determinize function added yay

--- lib/automata.ex
+++ lib/automata.ex
@@ -1,18 +1,68 @@
 defmodule Automata do
-  @moduledoc """
-  Documentation for `Automata`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Automata.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defmodule NFA do
+  defstruct states: MapSet.new(),
+            alphabet: MapSet.new(),
+            delta: %{},
+            start: nil,
+            accept: MapSet.new()
   end
+  defmodule DFA do
+  defstruct states: MapSet.new(),
+            alphabet: MapSet.new(),
+            delta: %{},
+            start: nil,
+            accept: MapSet.new()
+end
+  def determinize(%NFA{alphabet: a, delta: delta, start: start} = nfa) do
+  start_dfa = MapSet.new([start])
+  queue = :queue.from_list([start_dfa])
+  visited = MapSet.new([start_dfa])
+  delta_dfa = %{}
+
+  {states_dfa, delta_dfa} = bfs(queue, visited, delta_dfa, a, delta)
+
+  accept =
+    states_dfa
+    |> Enum.filter(fn set ->
+      not MapSet.disjoint?(set, nfa.accept)
+    end)
+    |> MapSet.new()
+
+  %DFA{
+    states: states_dfa,
+    alphabet: a,
+    delta: delta_dfa,
+    start: start_dfa,
+    accept: accept
+  }
+end
+  def bfs(queue,visited, delta_dfa,a,delta) do
+    case :queue.out(queue) do
+      {:empty,_} ->
+        {visited, delta_dfa}
+      {{:value,state},new_queue} ->
+        {new_delta_dfa,new_visited,updated_queue} = Enum.reduce(a,{delta_dfa,visited,new_queue},fn sym, {delta_acc,visited_acc,queue_acc} ->
+          T = move(state,sym,delta)
+          delta_acc = Map.put(delta_acc,{state,sym},T)
+
+          if(MapSet.member?(visited_acc, T)) do
+            {delta_acc,visited_acc,queue_acc}
+          else
+            {
+            delta_acc,
+            MapSet.put(visited_acc,T),
+            :queue.in(T,queue_acc)
+            }
+          end
+          end)
+
+        bfs(updated_queue,new_visited,new_delta_dfa,a,delta)
+    end
+  end
+  def move(S,sym,delta) do
+    Enum.reduce(S,MapSet.new(),fn s, acc ->
+      MapSet.union(Map.get(delta,{s,sym},MapSet.new()),acc)
+    end)
+  end
+
 end
--- test/automata_test.exs
+++ test/automata_test.exs
@@ -1,8 +1,36 @@
 defmodule AutomataTest do
   use ExUnit.Case
-  doctest Automata
 
-  test "greets the world" do
-    assert Automata.hello() == :world
+  alias Automata.{NFA, DFA}
+
+  test "determinize simple NFA" do
+    nfa = %NFA{
+      states: MapSet.new([:q0, :q1, :q2, :q3]),
+      alphabet: MapSet.new([:a, :b]),
+      start: :q0,
+      accept: MapSet.new([:q3]),
+      delta: %{
+        {:q0, :a} => MapSet.new([:q0, :q1]),
+        {:q0, :b} => MapSet.new([:q0]),
+        {:q1, :b} => MapSet.new([:q2]),
+        {:q2, :b} => MapSet.new([:q3])
+      }
+    }
+
+    dfa = Automata.determinize(nfa)
+
+    # 🔥 checks básicos
+    assert dfa.start == MapSet.new([:q0])
+
+    # debe existir el estado que contiene q3
+    assert Enum.any?(dfa.states, fn s ->
+      MapSet.member?(s, :q3)
+    end)
+
+    # prueba transición clave
+    s0 = MapSet.new([:q0])
+    s1 = MapSet.new([:q0, :q1])
+
+    assert Map.get(dfa.delta, {s0, :a}) == s1
   end
 end
